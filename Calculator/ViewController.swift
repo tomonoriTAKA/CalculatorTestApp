602273cb9a162ae3bb576d0c6c9cd99165467f00
Tomonori <tomonori0204.takahashi@gmail.com>
2017-01-23 17:50:20 +0900

    add ) calculate system

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,8 +8,30 @@
 
 import UIKit
 
+enum Operator{
+    
+    case undefined      //未定義
+    
+    case addition       //加法
+    
+    case subtraction    //減法
+    
+    case multiplication //乗法
+    
+    case division       //除法
+}
+
+
+
 class ViewController: UIViewController {
 
+    var firstValue = 0
+    
+    var secondValue = 0
+    
+    var currentOperator = Operator.undefined
+    
+    
     @IBOutlet weak var label: UILabel!
     
     override func viewDidLoad() {
@@ -23,15 +45,86 @@ class ViewController: UIViewController {
     }
 
     @IBAction func numberButtonTapped(_ sender: UIButton) {
+        var value = 0
+        switch sender.currentTitle! {
+        case "0":
+            value = 0
+        case "1":
+            value = 1
+        case "2":
+            value = 2
+        case "3":
+            value = 3
+        case "4":
+            value = 4
+        case "5":
+            value = 5
+        case "6":
+            value = 6
+        case "7":
+            value = 7
+        case "8":
+            value = 8
+        case "9":
+            value = 9
+        default:
+            value = 0
+        }
+        
+        if currentOperator == .undefined{
+            firstValue = firstValue * 10 + value
+            label.text = "\(firstValue)"
+        }else{
+            secondValue = secondValue * 10 + value
+            label.text = "\(secondValue)"
+        }
     }
 
     @IBAction func operatorButtonTapped(_ sender: UIButton) {
+        switch sender.currentTitle! {
+        case "+":
+            currentOperator = .addition
+        case "-":
+            currentOperator = .subtraction
+        case "✕":
+            currentOperator = .multiplication
+        case "÷":
+            currentOperator = .division
+        default:
+            currentOperator = .undefined
+        }
     }
     
     @IBAction func equalButtonTapped(_ sender: UIButton) {
+        var value = 0
+        
+        switch currentOperator {
+        case .addition:
+            value = firstValue + secondValue
+        case .subtraction:
+            value = firstValue - secondValue
+        case .multiplication:
+            value = firstValue * secondValue
+        case .division:
+            value = firstValue / secondValue
+        case .undefined:
+            value = firstValue
+        }
+        
+    label.text = "\(value)"
+    firstValue = 0
+    secondValue = 0
+    currentOperator = .undefined
+
     }
     
+    
+    
     @IBAction func allClearButtonTapped(_ sender: UIButton) {
+        firstValue = 0
+        secondValue = 0
+        currentOperator = .undefined
+        label.text = "0"
     }
     
 }
