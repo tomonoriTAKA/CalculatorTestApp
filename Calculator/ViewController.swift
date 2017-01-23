264fe164b3252c00bc72d45732aeea2c33601b90
Tomonori <tomonori0204.takahashi@gmail.com>
2017-01-23 16:05:23 +0900

    label and buttons were  connected to ViewController

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func numberButtonTapped(_ sender: UIButton) {
+    }
 
+    @IBAction func operatorButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func equalButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func allClearButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
